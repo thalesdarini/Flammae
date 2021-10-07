5345ab59ea0117f4be281c417790aa17614885ea
BatsBatsQrz <g.batistella@yahoo.com.br>
2021-10-07 01:27:30 -0300

    TowerBuilder working
    tower builder ta funcionando, com uma UI bem porca e um tipo de torre só, sem cobrar nada de almas, mas a torre ainda nao faz nada

---
+++ Assets/Scripts/TowerBuilder.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerBuilder : MonoBehaviour
+{
+    [SerializeField] GameObject towerCerberus;
+    [SerializeField] GameObject indicatorToBuild;
+    [SerializeField] GameObject pressToBuildUI;
+    [SerializeField] GameObject chooseTowerUI;
+    Renderer areaRenderer;
+    bool playerInArea;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerInArea = false;
+        areaRenderer = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(playerInArea)
+        {
+            if(Input.GetKeyDown(KeyCode.E))
+            {
+                pressToBuildUI.SetActive(false);
+                chooseTowerUI.SetActive(true);
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D objectThatEntered)
+    {
+        if (objectThatEntered.tag == "Player")
+        {
+            areaRenderer.enabled = true;
+            pressToBuildUI.SetActive(true);
+            playerInArea = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D objectThatExited)
+    {
+        if (objectThatExited.tag == "Player")
+        {
+            areaRenderer.enabled = false;
+            pressToBuildUI.SetActive(false);
+            if(chooseTowerUI.activeSelf)
+            {
+                chooseTowerUI.SetActive(false);
+            }
+            playerInArea = false;
+        }
+    }
+
+    public void BuildTowerCerberus()
+    {
+        chooseTowerUI.SetActive(false);
+        Destroy(indicatorToBuild);
+        Instantiate(towerCerberus, indicatorToBuild.transform.position + Vector3.up*(towerCerberus.transform.localScale.y - indicatorToBuild.transform.localScale.y)/2, indicatorToBuild.transform.rotation);
+        Destroy(transform.parent.gameObject);
+    }
+}

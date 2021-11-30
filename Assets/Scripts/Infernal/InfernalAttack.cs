159580391525e3ce21e504bd86dfc7ed2cdc3603
Thales Darini Souza <59424479+thalesdarini@users.noreply.github.com>
2021-11-30 14:13:08 -0300

    infernal attack

--- Assets/Scripts/Infernal/InfernalAttack.cs
+++ Assets/Scripts/Infernal/InfernalAttack.cs
@@ -1,11 +1,53 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class InfernalAttack : AttackScript
 {
+    [Header("Attack")]
+    [SerializeField] float attackRate;
+
+    // current state variables
+    bool isAttacking = false;
+    GameObject currentTarget = null;
+
+    // cached variables
+    Animator animator;
+
+    public bool IsAttacking { get => isAttacking; }
+
     override protected void Start()
     {
         base.Start();
+        animator = GetComponent<Animator>();
+        animator.SetFloat("m_attack", 1 / attackRate);
+    }
+
+    public void StartAttacking(GameObject enemy)
+    {
+        isAttacking = true;
+        currentTarget = enemy;
+        animator.SetBool("attacking", true);
+    }
+
+    private void Update()
+    {
+        if (currentTarget == null)
+        {
+            StopAttacking();
+        }
+    }
+
+    private void StopAttacking()
+    {
+        isAttacking = false;
+        animator.SetBool("attacking", false);
+    }
+
+    // called from animation
+    public void Attack()
+    {
+        currentTarget?.GetComponent<EnemyHealth>().TakeDamage(meleeDamage);
     }
 }
--- Assets/Scripts/Infernal/InfernalMovement.cs
+++ Assets/Scripts/Infernal/InfernalMovement.cs
@@ -1,11 +1,90 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class InfernalMovement : MovementScript
 {
+    [Header("Find Enemy")]
+    [SerializeField] ContactFilter2D enemyLayer;
+    [SerializeField] float findRange;
+    [SerializeField] float closestDistanceToEnemy;
+    float findTickRate = 0.4f;
+
+    // current state variables
+    float findTimeCounter = 0f;
+    GameObject enemyFound = null;
+
+    // cached references
+    InfernalAttack infernalAttack;
+
     override protected void Start()
     {
         base.Start();
+        infernalAttack = GetComponent<InfernalAttack>();
+    }
+
+    private void Update()
+    {
+        if (enemyFound == null)
+        {
+            if(!infernalAttack.IsAttacking)
+                LookForEnemy();
+        }
+        else
+        {
+            WalkToEnemy();
+        }
+    }
+
+    private void LookForEnemy()
+    {
+        findTimeCounter += Time.deltaTime;
+        if (findTimeCounter > findTickRate)
+        {
+            findTimeCounter = 0f;
+
+            Collider2D[] results = new Collider2D[5];
+            int n = Physics2D.OverlapCircle(transform.position, findRange, enemyLayer, results);
+            if (n != 0)
+            {
+                int closest = 0;
+                float closestDistance = Mathf.Infinity;
+                for (int i = 0; i < n; i++)
+                {
+                    if (results[i] != null)
+                    {
+                        float aux = Vector2.Distance(transform.position, results[i].transform.position);
+                        if (aux < closestDistance)
+                        {
+                            closest = i;
+                            closestDistance = aux;
+                        }
+                    }
+                }
+                enemyFound = results[closest].gameObject;
+            }
+        }
+    }
+
+    private void WalkToEnemy()
+    {
+        Vector2 currentPosition = transform.position;
+        Vector2 targetPosition = enemyFound.transform.position;
+
+        if (Vector2.Distance(currentPosition, targetPosition) > closestDistanceToEnemy)
+            transform.position = Vector2.MoveTowards(currentPosition, targetPosition, movementSpeed * Time.deltaTime);
+        else if (!infernalAttack.IsAttacking)
+            infernalAttack.StartAttacking(enemyFound);
+
+        // rotate
+        if (targetPosition.x > currentPosition.x)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+        else
+        {
+            transform.rotation = Quaternion.Euler(0, -180, 0);
+        }
     }
 }
